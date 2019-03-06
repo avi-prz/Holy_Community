db57d453642545de67f426cf7562115125c802ea
avi-prz <avi.prz@gmail.com>
2019-03-06 15:36:19 +0200

    creating project skeleton

---
+++ app/app.js
@@ -0,0 +1,8 @@
+var app = angular.module('holyCommunityApp', ['ngRoute']);/*.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "main.html",
+        controller: "communityCtrl"
+    }).otherwise({
+        redirectTo: "/"
+    });
+});*/
